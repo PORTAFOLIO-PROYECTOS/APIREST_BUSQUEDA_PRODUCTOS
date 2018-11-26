8cd7a88e29686691135e300a103285cf22208231
HugoRoca <hugo.rock20@hotmail.com>
2018-11-26 11:46:37 -0500

    more changes

--- app/controllers/baseController.js
+++ app/controllers/baseController.js
@@ -1,6 +1,8 @@
 const buscadorRepository = require("../repository/buscadorRepository"),
     parametrosSalida = require("../models/buscador/parametrosSalida"),
+    parametrosFiltro = require("../models/buscador/parametrosFiltro"),
     utils = require("../common/utils"),
+    config = require("../../config"),
     redis = require("../common/redis"),
     sql = require("../common/sql");
 
@@ -23,21 +25,62 @@ var baseController = (function () {
         }
         return JSON.parse(dataRedis);
     }
+    
+    /**
+     * Ejecuta la query de ES
+     * @param {array} parametros - Parametros que recibe el API
+     * @param {array} rangosRedis - Datos de redis
+     */
+    async function ejecutarElasticsearch(parametros, rangosRedis) {
+        return new Promise((resolve, reject) => {
+            buscadorRepository.buscar(parametros, rangosRedis).then((resp) => {
+                resolve(resp);
+            }, (err) => {
+                console.log('Error: al consultar ES');
+                reject(err);
+            });
+        });
+    }
 
+    /**
+     * Devuelve json de productos validados con el STOCK
+     * @param {array} SAPs - Codigos SAPs a validar
+     * @param {string} isoPais - ISO del país
+     * @param {int} diaFacturacion - Día de facturación 
+     * @param {array} productos - Lista de productos a validar
+     */
+    async function validarStock(SAPs, isoPais, diaFacturacion, productos){
+        if (config.flags.validacionStock && diaFacturacion >= 0) {
+            
+            let dataStock = await stockRepository.Validar(SAPs, isoPais);
+    
+            //- Paso 5.2: Validación datos stock
+            for (const i in dataStock) {
+                for (const j in productos) {
+                    if (dataStock[i].codsap == productos[j].SAP) {
+                        productos[j].Stock = dataStock[i].estado == 1 ? true : false;
+                        break;
+                    }
+                }
+            }
+        }
+        return productos;
+    }
+    
     /**
      * Devuelve array con los productos validados
      * @param {json} data - Resultado de la consulta de ES
      * @param {array} parametros - Parametros que recibe el api
      * @param {array} SAPs - Retorno de codigos SAPS
      */
-    function devuelveJSONProductos(data, parametros, SAPs){
+    function devuelveJSONProductos(data, parametros, SAPs) {
         let productos = [];
 
         for (const key in data.hits.hits) {
             const element = data.hits.hits[key],
                 source = element._source,
                 imagen = utils.getUrlImagen(source.imagen, parametros.codigoPais, source.imagenOrigen, parametros.codigoCampania, source.marcaId);
-    
+
             productos.push(
                 new parametrosSalida(
                     source.cuv,
@@ -55,7 +98,7 @@ var baseController = (function () {
                     true,
                     source.estrategiaId
                 ));
-    
+
             if (SAPs.indexOf(source.codigoProducto) < 0 && (source.codigoProducto != undefined || source.codigoProducto != null)) {
                 SAPs.push(source.codigoProducto);
             }
@@ -65,29 +108,76 @@ var baseController = (function () {
     }
 
     /**
-     * Ejecuta la query de ES
-     * @param {array} parametros - Parametros que recibe el API
-     * @param {array} rangosRedis - Datos de redis
+     * Devuelve en formato JSON todos los filtros, aunque tengan ceros
+     * @param {array} data - Resultado de la consulta de ES
+     * @param {array} dataRedis - Resultado de la consulta de REDIS
      */
-    async function ejecutarElasticsearch(parametros, rangosRedis) {
-        return new Promise((resolve, reject) => {
-            buscadorRepository.buscar(parametros, rangosRedis).then((resp) => {
-                resolve(resp);
-            }, (err) => {
-                console.log('Error: al consultar ES');
-                reject(err);
-            });
-        });
-    }
+    function devuelveJSONFiltros(data, dataRedis) {
+        let preciosRedis = utils.selectInArray(dataRedis, config.constantes.codigoFiltroPrecio),
+            marcasRedis = utils.selectInArray(dataRedis, config.constantes.codigoFiltroMarca),
+            categoriasRedis = utils.selectInArray(dataRedis, config.constantes.codigoFiltroCategoria),
+            categoriasES = data.aggregations.categoriasFiltro.buckets,
+            marcasES = data.aggregations.marcasFiltro.buckets,
+            preciosES = data.aggregations.preciosFiltro.buckets,
+            categorias = [],
+            marcas = [],
+            precios = [];
 
-    function devuelveJSONFiltros(data, filtros){
-        
+
+        for (const i in categoriasRedis) {
+            const element = categoriasRedis[i];
+            const dataES = categoriasES.find(x => x.key == element.Nombre);
+            const dataEntrada = parametros.filtroCategoria.find(x => x.idFiltro == element.Descripcion);
+            categorias.push(
+                new parametrosFiltro(
+                    element.Descripcion,
+                    element.Nombre,
+                    dataES == undefined ? 0 : dataES.doc_count,
+                    dataEntrada == undefined ? false : true
+                ));
+        }
+
+        for (const i in marcasRedis) {
+            const element = marcasRedis[i];
+            const dataEntrada = parametros.filtroMarca.find(x => x.idFiltro.toLowerCase() == element.Descripcion.toLowerCase());
+            const dataES = marcasES.find(x => x.key == element.Nombre);
+
+            marcas.push(
+                new parametrosFiltro(
+                    element.Descripcion,
+                    element.Nombre,
+                    dataES == undefined ? 0 : dataES.doc_count,
+                    dataEntrada == undefined ? false : true
+                ));
+        }
+
+        for (const i in preciosRedis) {
+            const element = preciosRedis[i];
+            const dataES = preciosES.find(x => x.key == element.Nombre);
+            const dataEntrada = parametros.filtroPrecio.find(x => x.idFiltro == element.Descripcion);
+            precios.push(
+                new parametrosFiltro(
+                    element.Descripcion,
+                    element.Nombre,
+                    dataES == undefined ? 0 : dataES.doc_count,
+                    dataEntrada == undefined ? false : true
+                ));
+        }
+
+        return {
+            categorias,
+            marcas,
+            precios
+        }
+
     }
 
     return {
         obtenerDatosRedis: obtenerDatosRedis,
         ejecutarElasticsearch: ejecutarElasticsearch,
-        devuelveJSONProductos: devuelveJSONProductos
+        devuelveJSONProductos: devuelveJSONProductos,
+        devuelveJSONFiltros: devuelveJSONFiltros,
+        validarStock: validarStock
     }
 })();
 
--- app/controllers/buscadorController.js
+++ app/controllers/buscadorController.js
@@ -1,101 +1,21 @@
-const parametrosEntrada = require("../models/buscador/parametrosEntrada"),
-    parametrosSalida = require("../models/buscador/parametrosSalida"),
-    parametrosFiltro = require("../models/buscador/parametrosFiltro"),
-    
+const parametrosEntrada = require("../models/buscador/parametrosEntrada"),    
     stockRepository = require("../repository/stockRepository"),
-    utils = require("../common/utils"),
     config = require("../../config"),
     baseController = require("../controllers/baseController");
 
 async function ejecutar(parametros) {
-    //- Paso 1: Consultar Redis para obtener los filtros
-    let categorias = [];
-    let marcas = [];
-    let precios = [];
+    
     let name = `${config.ambiente}_${config.name}_${parametros.codigoPais}_FiltrosDelBuscador`;
     let dataRedis = await baseController.obtenerDatosRedis(name, parametros.codigoPais);
-    let preciosRedis = utils.selectInArray(dataRedis, config.constantes.codigoFiltroPrecio);
-    let marcasRedis = utils.selectInArray(dataRedis, config.constantes.codigoFiltroMarca);
-    let categoriasRedis = utils.selectInArray(dataRedis, config.constantes.codigoFiltroCategoria);
     let dataElastic = await baseController.ejecutarElasticsearch(parametros, preciosRedis);
-
     let productos = [],
         SAPs = [],
         filtros = [],
         total = dataElastic.hits.total;
 
     productos = baseController.devuelveJSONProductos(dataElastic, parametros, SAPs);
-
-    const categoriasES = dataElastic.aggregations.categoriasFiltro.buckets;
-
-    for (const i in categoriasRedis) {
-        const element = categoriasRedis[i];
-        const dataES = categoriasES.find(x => x.key == element.Nombre);
-        const dataEntrada = parametros.filtroCategoria.find(x => x.idFiltro == element.Descripcion);
-        categorias.push(
-            new parametrosFiltro(
-                element.Descripcion,
-                element.Nombre,
-                dataES == undefined ? 0 : dataES.doc_count,
-                dataEntrada == undefined ? false : true
-            ));
-    }
-
-    //-- Paso 4.3: datos => aggregations marcasFiltro
-    const marcasES = dataElastic.aggregations.marcasFiltro.buckets;
-
-    for (const i in marcasRedis) {
-        const element = marcasRedis[i];
-        const dataEntrada = parametros.filtroMarca.find(x => x.idFiltro.toLowerCase() == element.Descripcion.toLowerCase());
-        const dataES = marcasES.find(x => x.key == element.Nombre);
-
-        marcas.push(
-            new parametrosFiltro(
-                element.Descripcion,
-                element.Nombre,
-                dataES == undefined ? 0 : dataES.doc_count,
-                dataEntrada == undefined ? false : true
-            ));
-    }
-
-    //-- Paso 4.4: datos => aggregations preciosFiltro
-    const preciosES = dataElastic.aggregations.preciosFiltro.buckets;
-
-    for (const i in preciosRedis) {
-        const element = preciosRedis[i];
-        const dataES = preciosES.find(x => x.key == element.Nombre);
-        const dataEntrada = parametros.filtroPrecio.find(x => x.idFiltro == element.Descripcion);
-        precios.push(
-            new parametrosFiltro(
-                element.Descripcion,
-                element.Nombre,
-                dataES == undefined ? 0 : dataES.doc_count,
-                dataEntrada == undefined ? false : true
-            ));
-    }
-
-    //- Asignación Filtros 
-    filtros = {
-        categorias,
-        marcas,
-        precios
-    }
-
-    //- Paso 5: Validación de stock PROL
-    if (total > 0 && config.flags.validacionStock && parametros.diaFacturacion >= 0) {
-        //- Paso 5.1: Consulta APIPROL
-        let dataStock = await stockRepository.Validar(SAPs, parametros.codigoPais);
-
-        //- Paso 5.2: Validación datos stock
-        for (const i in dataStock) {
-            for (const j in productos) {
-                if (dataStock[i].codsap == productos[j].SAP) {
-                    productos[j].Stock = dataStock[i].estado == 1 ? true : false;
-                    break;
-                }
-            }
-        }
-    }
+    productos = baseController.validarStock(SAPs, parametros.codigoPais, parametros.diaFacturacion, productos);
+    filtros = baseController.devuelveJSONFiltros(dataElastic, dataRedis);
 
     return {
         total: total,
@@ -103,8 +23,6 @@ async function ejecutar(parametros) {
         filtros: filtros
     }
 }
-
-
 
 function validarFiltro(val) {
     let array = [];
