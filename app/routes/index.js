3d0626addf1c6f991bca1badfce958798bace47c
HugoRoca <hugo.rock20@hotmail.com>
2018-11-27 14:26:23 -0500

    asd index.html

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("", (req, res) => {
-    let ruta = __dirname.replace("app/routes", "index.html");
+    let ruta = __dirname.replace(`app\\routes`, "index.html");
     console.log(ruta);
     res.sendFile(ruta);
 });
